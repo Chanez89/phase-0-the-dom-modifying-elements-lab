d22de85a48ba3fad2300785c82690087a99ba77a
NeilChanezGracia <chanez89@hotmail.com>
2021-08-20 12:16:26 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+main.remove(); //test 1 
+
+const newHeader = document.createElement('h1');// test 2
+
+newHeader.id = 'victory'; // test 3
+
+newHeader.innerHTML = "<h1>Neil is the champion</h1>"; // test 4 
