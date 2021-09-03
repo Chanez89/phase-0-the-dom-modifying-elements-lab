3555622fd5077904178a36218371bdbe70d7dd79
Neil B. Chanez Gracia <Chanez89@Hotmail.com>
2021-09-02 22:56:23 -0700

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,69 @@
-// Write your code here!
+//test1
+document.getElementById('main').remove();
 
-main.remove(); //test 1 
 
-const newHeader = document.createElement('h1');// test 2
+//test2
 
-newHeader.id = 'victory'; // test 3
+const newHeader = document.createElement('h1');
 
-newHeader.innerHTML = "<h1>Neil is the champion</h1>"; // test 4 
+newHeader.id = 'victory';
+
+
+
+
+
+newHeader.textContent = "NEILIOTHEDEALIO is the champion";
+
+document.body.append(newHeader);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Write your code here!
+
+// main.remove(); //test 1 
+
+// const newHeader = document.createElement('h1');// test 2
+
+// newHeader.id = 'victory'; // test 3
+
+// newHeader.innerHTML = "<h1>Neil is the champion</h1>"; // test 4 
